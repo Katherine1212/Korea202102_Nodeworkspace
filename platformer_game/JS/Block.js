f2e5b4e43846e1965a37454f119d2c602623ecb2
Katherine1212 <katherinekim971212@gmail.com>
2021-04-05 22:20:25 +0900

    Revert "-"
    This reverts commit 0853065aa44b0a54822b3cf4179f867092252241.

--- platformer_game/JS/Block.js
+++
@@ -1,9 +0,0 @@
-// 블럭을 정의
-class Block extends GameObject{
-    constructor(container,src,width,height,x,y,velX,velY){
-        super(container,src,width,height,x,y,velX,velY); // 부모의 함수
-        
-
-
-    }
-}
